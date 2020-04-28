8a6d165ba0ef567c0352806627c3f403636061cf
newestdev <sree.thevireen@gmail.com>
2020-04-28 08:27:48 -0700

    commit

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input,EventEmitter, Output } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
@@ -6,14 +6,10 @@ import { Component,Input,EventEmitter, Output } from '@angular/core';
   styleUrls: ['./media-item.component.css']
 })
 export class MediaItemComponent {
-    @Input() mediaItem;
-    @Output() delete = new EventEmitter();
+  @Input() mediaItem;
+  @Output() delete = new EventEmitter();
 
-    onDelete()
-    {
-        console.log("Deleted!");
-        this.delete.emit(this.mediaItem);
-        console.log(this.mediaItem);
-    }
-    
+  onDelete() {
+    this.delete.emit(this.mediaItem);
+  }
 }
